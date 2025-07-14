fca47f0d932d61621573947be9d5dd29b9631395
Karim <gtder41@gmail.com>
2025-07-14 21:31:21 +0500

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './Header.css'
-import link from '/img/Link.png'
+import React from "react";
+import "./Header.css";
+import link from "/img/Link.png";
+import seach from "/img/search.png";
 export default function Header() {
-	return (
-
-			<div className="header">
-				<div className="header__left">
-					<img src={link} alt="link" className="header__left-link"/>
-				</div>
+  return (
+    <div className="header">
+      <img src={link} alt="link" className="header-link" />
+			<div className="header-search">
+				<img src={seach} alt="" />
+				<input type="text" placeholder="Search usernames" className="header-search-input" />
 			</div>
-	)
+    </div>
+  );
 }
